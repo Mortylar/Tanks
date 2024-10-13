2c092568fdf2236993a179ec18a3c8c620900b2b
Morty Laree <mortylar@si-d8.nsk.21-school.ru>
2024-10-13 18:17:10 +0700

    init

---
+++ TanksClient/src/main/java/edu/school21/tanks/App.java
@@ -0,0 +1,11 @@
+package edu.school21.tanks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
